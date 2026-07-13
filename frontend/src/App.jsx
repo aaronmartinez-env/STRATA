e8ffca62e2eec0450bbb59cf8d63734df09650f6
Aaron Martinez <aaronmartinez.env@gmail.com>
2026-07-13 11:28:30 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ export default function App() {
       <Route path="/" element={<RootLayout />}>
         <Route index element={<Home />} />
         <Route path="live" element={<Live />} />
+        <Route path="wind" element={<Wind />} />
         <Route path="case-studies/valencia-2021-2022" element={<CaseStudyValencia />} />
         <Route path="case-studies/dana-2024" element={<CaseStudyDana />} />
       </Route>
