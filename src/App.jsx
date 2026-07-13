0df86e4d097d3d2a1e64a9483f7ba5e94bb31fcf
Aaron Martinez <aaronmartinez.env@gmail.com>
2026-07-13 09:45:54 +0200

    Update App.jsx (Wind)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import Live from './pages/Live';
 import CaseStudyValencia from './pages/CaseStudyValencia';
 import CaseStudyDana from './pages/CaseStudyDana';
+import Wind from './pages/Wind';
+
 
 export default function App() {
   return (
@@ -13,6 +15,7 @@ export default function App() {
         <Route path="live" element={<Live />} />
         <Route path="case-studies/valencia-2021-2022" element={<CaseStudyValencia />} />
         <Route path="case-studies/dana-2024" element={<CaseStudyDana />} />
+        <Route path="wind" element={<Wind />} />
       </Route>
     </Routes>
   );
